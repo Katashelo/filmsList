2436a992fb196c3b2e3f4415ba9f4eef246d92d3
Katashelo <katashelo@gmail.com>
2023-08-21 09:46:35 +0200

    pagination

--- src/FilmsPopular111.js
+++ src/FilmsPopular111.js
@@ -5,36 +5,31 @@ import useFetchMovies from "./useFetchMovies";
 
 const FilmsPopulars111 = (props) => {
 
-    const [isToggleOn, setIsToggleOn] = useState(true)
-    const url = "https://api.themoviedb.org/3/movie/popular?api_key=5be5ccee10320c70ef1f2aeeb98d3a41&language=en-US&page=1"
+
+    const url = "https://api.themoviedb.org/3/movie/popular?api_key=5be5ccee10320c70ef1f2aeeb98d3a41&language=en-US"
     const { items, isLoaded, error, currentPage, totalPage } = useFetchMovies(url)
 
-    const handleClick = () => {
-        if (isToggleOn === true) {
-            setIsToggleOn(false);
-        } else {
-            setIsToggleOn(true)
-        }
 
-    }
     if (error) {
         return <div>Помилка: {error.message}</div>;
     } else if (!isLoaded) {
         return <div>Завантаження...</div>;
     } else {
 
-        const imagePath = "https://image.tmdb.org/t/p/w300"
+     
 
-        const Ranking = (props) => {
-            return <button onClick={handleClick}>
-                {isToggleOn ? "Rate Switch" : props.rnk + " " + 'Hide Rate'}
-            </button>
-        }
+        
+        const itemy = []
+       items.items.map(arr => (
+        itemy.push(arr)
+       ))
+       
+        console.log(itemy.length, 'dlina')
         return (
             <div>
                 <h1 className='title' > Favourite Movies </h1>
-                <Pagination />
-                <ul className='app-wrapper' >
+                <Pagination elements={itemy.length} items={items.items}/>
+                {/* <ul className='app-wrapper' >
                     {items && items.items.map(item => (
                         <li key={item.id}>
                             <div  >{item.title} </div>
@@ -45,7 +40,7 @@ const FilmsPopulars111 = (props) => {
                             <div className='overview' >{item.overview}</div>
                         </li>
                     ))}
-                </ul>
+                </ul> */}
             </div>
         );
     }
